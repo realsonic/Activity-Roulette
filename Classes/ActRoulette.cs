6b2f145dc883cdbb20a6879669743a7f5cb6f14c
Igor Zilinskiy <igor.zilinskiy@atos.net>
2022-08-03 17:22:53 +0300

    Add project files.

---
+++ Classes/ActRoulette.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_Roulette.Classes
+{
+    class Roulette
+    {
+        // Папка приложения
+        private static string AppFolder = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Activity Roulette";
+        // Файл активностей
+        private static string filePath = AppFolder + @"\activitylist.txt";
+        public static void CreateAppFolder()
+        {
+            if (Directory.Exists(AppFolder))
+            {
+                return;
+            }
+            else
+            {
+                Directory.CreateDirectory(AppFolder);
+            }
+        }
+        public static void CreateActivityList()
+        {
+            if (File.Exists(filePath))
+            {
+                return;
+            }
+            else
+            {
+                FileStream fileCreator = File.Create(filePath);
+                // fileCreator.Close();
+            }
+        }
+        public static void AddNewActivity()
+        {
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() != "exit")
+                {
+                    using (StreamWriter activityWriter = File.AppendText(filePath))
+                    {
+                        activityWriter.WriteLine(userInput);
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Activity_Roulette.Classes;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_Roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User, Hi!");
+            Roulette.CreateAppFolder();
+            Roulette.CreateActivityList();
+            Roulette.AddNewActivity();
+            /* Запрос на добавление новых активностей
+            if (userAnswer)
+            {
+                Roulette.AddNewActivity()
+            }
+            else
+            {
+                return;
+            }*/
+            Console.ReadLine();
+        }
+    }
+}
