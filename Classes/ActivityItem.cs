d9dc23eb83bd179ada1e9e991f074908e86f63af
Igor Zilinskiy <igor.zilinskiy@atos.net>
2022-08-09 19:18:04 +0300

    osmyslenniy commit :))

--- Classes/ActivityItem.cs
+++ Classes/ActivityItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,13 +16,13 @@ namespace Activity_Roulette.Classes
         public int activityListItemWeight;
 
         // вес активности относительно файла активностей определяется путём деления activityListItemWeight на сумму activityListItemWeight всех активностей в файле. А int ли это вообще???
-        public int relativeActivtyListItemWeight;
+        public float relativeActivtyListItemWeight;
 
         // вес активности относительно файла с логами определяется путём деления количества попаданий активности в этот самый лог на общее число записей в нём. А int ли это вообще???
-        public int relativeLogFileItemWeight;
+        public float relativeLogFileItemWeight;
 
         // относительный невыбранный вес активности определяется путём вычитания relativeLogFileItemWeight из relativeActivtyListItemWeight. А int ли это вообще???
-        public int nonSelectionRelativeItemWeight;
+        public float nonSelectionRelativeItemWeight;
 
         // Допилить конструктор???
         public ActivityItem(string fileLine)
@@ -30,5 +31,11 @@ namespace Activity_Roulette.Classes
             name = basis[0];
             activityListItemWeight = Int32.Parse(basis[1]);
         }
+        public static void SetRelativeActivtyListItemWeight(ActivityItem activityItem, ActivityItemsList list)
+        {
+            float overallActivityListWeight = list.GetOverallActivityListWeight();
+            activityItem.relativeActivtyListItemWeight = activityItem.activityListItemWeight / overallActivityListWeight;
+        }
+
     }
 }
---
+++ Classes/ActivityItemsList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_Roulette.Classes
+{
+    class ActivityItemsList
+    {
+        // Контейнер текущих активностей
+        public static Dictionary<string, ActivityItem> activityItemsContainer;
+        // Общий вес файла активностей
+        private static float overallActivityListWeight;
+
+        public ActivityItemsList()
+        {
+            Dictionary<string, ActivityItem> Container = new Dictionary<string, ActivityItem>();
+            foreach (string line in File.ReadLines(Roulette.activityListPath))
+            {
+                ActivityItem activity = new ActivityItem(line);
+                Container.Add(activity.name, activity);
+            }
+            activityItemsContainer = Container;
+        }
+        public Dictionary<string, ActivityItem> GetActivityItemsContainer()
+        {
+            return activityItemsContainer;
+        }
+        public void SetOverallActivityListWeight(Dictionary<string, ActivityItem> Container)
+        {
+            int sum = 0;
+            foreach (KeyValuePair<string, ActivityItem> activity in Container)
+            {
+                sum += activity.Value.activityListItemWeight;
+            }
+            overallActivityListWeight = sum;
+        }
+        public float GetOverallActivityListWeight()
+        {
+            return overallActivityListWeight;
+        }
+    }
+
+}
